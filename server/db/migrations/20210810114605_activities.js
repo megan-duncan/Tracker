9aeebe89108f651fe75b4d439745a7171e159392
Megan <megan.duncan@gmail.com>
2021-08-10 12:40:55 +1200

    write seeds

--- server/db/migrations/20210810114605_activities.js
+++ server/db/migrations/20210810114605_activities.js
@@ -2,6 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('activities', table => {
     table.increments('id')
     table.integer('habits_id')
+    table.date('date')
     table.foreign('habits_id').references('id').inTable('habits')
   })
 }
